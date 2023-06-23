d296ec3aabc89a14162c28369eaab7162e111e03
Joice Eglair <116314841+joiceeglair@users.noreply.github.com>
2023-06-23 11:43:45 -0300

    Create styles.ts

---
+++ src/components/Cabecalho/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+import { Cores } from '../../styles'
+
+export const CabecalhorH = styled.header`
+  background-color: ${Cores.corSecundaria};
+  color: ${Cores.corPrincipal};
+  text-align: center;
+  padding: 24px 0;
+`
